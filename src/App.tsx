1eb46a64af5267f37e535ec7f80cc6a1670886f8
Richy Johannes <102476191+RichyJohanness@users.noreply.github.com>
2025-09-27 08:33:21 +0000

    fe update

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Beranda from "./pages/Beranda";
-import TentangKami from "./pages/TentangKami";
-import VisiMisi from "./pages/VisiMisi";
-import Kontak from "./pages/Kontak";
-import Layanan from "./pages/Layanan"; // Import page Layanan
-import Education from "./pages/Education";
-import RisetKonsultasi from "./pages/RisetKonsultasi";
-import JurnalProofread from "./pages/JurnalProofread";
-import Penerbitan from "./pages/Penerbitan";
-import KonsultasiRelasi from "./pages/KonsultasiRelasi";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -30,17 +21,6 @@ const App = () => (
           <main className="flex-1">
             <Routes>
               <Route path="/" element={<Beranda />} />
-              <Route path="/tentang" element={<TentangKami />} />
-              <Route path="/visi-misi" element={<VisiMisi />} />
-              {/* Tambahkan rute untuk halaman Layanan */}
-              <Route path="/layanan" element={<Layanan />} />
-              <Route path="/education" element={<Education />} />
-              <Route path="/riset-konsultasi" element={<RisetKonsultasi />} />
-              <Route path="/jurnal" element={<JurnalProofread />} />
-              <Route path="/penerbitan" element={<Penerbitan />} />
-              <Route path="/konsultasi-relasi" element={<KonsultasiRelasi />} />
-              <Route path="/kontak" element={<Kontak />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
